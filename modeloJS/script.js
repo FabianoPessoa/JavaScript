0fa6cbe0a3b7052eb2ad3b3fa7d4948f3962f546
Fabiano Pessoa <70175879+FabianoPessoa@users.noreply.github.com>
2021-08-21 01:10:59 -0300

    Update script.js
    Commit github 4
    Ajustar a cor de fundo para ficar parecido com a cor de fundo das fotos.

--- modeloJS/script.js
+++ modeloJS/script.js
@@ -3,6 +3,18 @@ function carregar(){
   var msg = window.document.getElementById('msg');
   var img = window.document.getElementById('imagen');
   var data = new Date();
+  /* Comentar a hora abaixo e forc,ar outros hora'rios */
   var hora = data.getHours();
+  //var hora = 8;
   msg.innerHTML = `Agora são ${hora} horas`
+  if (hora >= 0 && hora < 12){
+    //Bom dia! 
+    /* img.src ='fotomanha.png'; */
+  }else if(hora <=12 && hora < 18){
+    //Boa tarde!
+    /* img.src ='fototarde.png'; */
+  }else{
+    //boa noite!
+    /* img.src ='fotonoite.png'; */
+  }
 }

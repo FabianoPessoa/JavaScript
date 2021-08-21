fb6990a238267505e8d2ffa6168246c7a626d390
Fabiano Pessoa <70175879+FabianoPessoa@users.noreply.github.com>
2021-08-21 01:01:20 -0300

    Update script.js
    Commit github 2

--- modeloJS/script.js
+++ modeloJS/script.js
@@ -1,1 +1,8 @@
-alert('Olá!');
+/* **** Comec,ando a configurar o JavaSript**** */
+function carregar(){
+  var msg = window.document.getElementById('msg');
+  var img = window.document.getElementById('imagen');
+  var data = new Date();
+  var hora = data.getHours();
+  msg.innerHTML = `Agora são ${hora} horas`
+}

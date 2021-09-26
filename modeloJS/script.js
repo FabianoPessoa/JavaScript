d6da7e84a2a54bbceab3deb9c53ef3e448d766d8
FabianoPessoa <fabiano.pesoa30@gmail.com>
2021-09-26 15:11:19 -0300

    Criado o Site com Configura as Horas do Dia

--- modeloJS/script.js
+++ modeloJS/script.js
@@ -1,20 +1,24 @@
 /* **** Comec,ando a configurar o JavaSript**** */
 function carregar(){
   var msg = window.document.getElementById('msg');
-  var img = window.document.getElementById('imagen');
+  var img = window.document.getElementById('imagem');
   var data = new Date();
   /* Comentar a hora abaixo e forc,ar outros hora'rios */
-  var hora = data.getHours();
-  //var hora = 8;
+  //var hora = data.getHours();
+  var hora = 19;
   msg.innerHTML = `Agora são ${hora} horas`
   if (hora >= 0 && hora < 12){
     //Bom dia! 
-    /* img.src ='fotomanha.png'; */
-  }else if(hora <=12 && hora < 18){
+    img.src ='fotomanha.jpg';
+		document.body.style.background = '#e2cd9f';
+  }else if(hora >=12 && hora <= 18){
     //Boa tarde!
-    /* img.src ='fototarde.png'; */
-  }else{
-    //boa noite!
-    /* img.src ='fotonoite.png'; */
-  }
+    img.src ='fototarde.jpg';
+		document.body.style.background = '#b9846f';
+	}else
+		{
+    //Boa noite!
+    img.src ='fotonoite.jpg';
+		document.body.style.background = '#515154';
+		}
 }

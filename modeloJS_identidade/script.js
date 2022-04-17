e4e31da1c2e670e56ddd58a1381ce9a3e092ec0f
FabianoPessoa <fabiano.pessoa30@gmail.com>
2022-04-16 22:26:35 -0300

    Continuacao script JS para identificacao de sexo

--- modeloJS_identidade/script.js
+++ modeloJS_identidade/script.js
@@ -1,14 +1,17 @@
-function verificar(){ 
-	window.alert('borao peridodo');
+function verificar(){
+	/* Variaveis*/
 	var data = new Date();
 	var ano = data.getFullYear();
 	var fano = document.getElementById('txtano');
 	var res = document.querySeletor('div#res');
-	if(fano.value.length == 0 || fano.value> ano ) { 
+
+	/* programa */
+	if(fano.value.length == 0 || Number(fano.value)> ano ) { 
 		window.alert('[ERRO]Verifique os dados');
 	}
 	else { 
-		window.alert('tudo ok');
+		var fsex = document.getElementById('radsex');
+		var idade = ano - Number(fano.value);
+		res.innerHTML = `Idade calculada: ${idade}`;
 		}
 }
-

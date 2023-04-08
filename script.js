c71e2701a00cc2e67405fc7337ca448833e3dbfb
FabianoPessoa <fabiano.pessoa30@gmail.com>
2023-04-07 22:37:57 -0300

    Revisar aula 9

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
-alert('Olá!');
+/* alert('Olá!'); */
+/* window.alert('Olá!'); */
+/***************************  
+* Exige entrada
+*/
+/* window.confirm('Olá!'); */
+/*****************************  
+* Exige entrada
+*/
+/* window.prompt('Olá!'); */
+/****************************
+* Não teria como representar*
+* as varáveis nos exemplos  *
+* então estudei na teoria   *
+*****************************/
+/*let ou const  */
+//var nome =  window.prompt('Qual nome?');
+//window.alert('Oi, '+nome);
+/******************************
+* var n1 = window.prompt('Escreva numero 1');
+* var n2 = window.prompt('Escreva numero 2');
+* var s = n1 + n2;
+* Não funciona pois são strings
+* var n1 = Number.parseInt(window.prompt('Escreva numero 1'));
+* var n2 = Number.parseInt(window.prompt('Escreva numero 2'));
+* Number.parseFloat();
+* var s = n1 + n2;
+* funciona pois são números
+*/
+/**********************************
+* Pode ser Simplificado para Number
+* var n1 = Number(window.prompt('Escreva numero 1'));
+* var n2 = Number(window.prompt('Escreva numero 2'));
+* var s = n1 + n2;
+* window.alert('soma '+s);
+*************************************/
+/*************************************
+* Para converter para String
+* String(<variavel>);
+* ou
+* <variavel>.toStrong();
+***************************************/
+/*****************************************
+* var n1 = Number(window.prompt('Escreva numero 1'));
+* var n2 = Number(window.prompt('Escreva numero 2'));
+* var s = n1 + n2;
+* window.alert(`Soma ${s}`);
+****************************************/
+var nome = window.prompt('Qual nome?');
+document.write(`O nome tem comprimento ${nome.length} letras`);
+
+
